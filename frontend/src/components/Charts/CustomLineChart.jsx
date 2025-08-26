8aea5dabbadb6b671cb6d5f81c8d767c03be5525
TRSiddique <tasfique.cse.21701008@gmail.com>
2025-08-26 13:54:01 +0600

    chart added

--- frontend/src/components/Charts/CustomLineChart.jsx
+++ frontend/src/components/Charts/CustomLineChart.jsx
@@ -18,7 +18,23 @@ const CustomLineChart = ({ data }) => {
 
   return (
     <div className="bg-white">
-      
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={data}>
+            <defs>
+            <linearGradient id="incomeGradient" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#875cf5" stopOpacity={0.4} />
+                <stop offset="95%" stopColor="#875cf5" stopOpacity={0} />
+            </linearGradient>
+            </defs>
+            
+            <CartesianGrid stroke="none" />
+            <XAxis dataKey="month" tick={{ fontSize: 12, fill: "#555" }} stroke="none" />
+            <YAxis tick={{ fontSize: 12, fill: "#555" }} stroke="none" />
+            <Tooltip content={<CustomTooltip />} />
+            
+            <Area type="monotone" dataKey="amount" stroke="#875cf5"  fill="url(#incomeGradient)" strokeWidth={3} dot={{ r: 3, fill: "#ab8df8" }} />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 };

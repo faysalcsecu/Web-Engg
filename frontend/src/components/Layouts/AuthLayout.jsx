da5031ec80516b038708df1cd69dbec3ac798b49
TRSiddique <tasfique.cse.21701008@gmail.com>
2025-09-06 20:47:09 +0600

    delete button niye kaj

--- frontend/src/components/Layouts/AuthLayout.jsx
+++ frontend/src/components/Layouts/AuthLayout.jsx
@@ -12,9 +12,9 @@ const AuthLayout = ({ children }) => {
       </div>
 
       <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
-        <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5" />
-        <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10" />
-        <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5" />
+        <div className="w-48 h-48 rounded-[40px] bg-green-600 absolute -top-7 -left-5" />
+        <div className="w-48 h-56 rounded-[40px] border-[20px] border-green-600 absolute top-[30%] -right-10" />
+        <div className="w-48 h-48 rounded-[40px] bg-green-500 absolute -bottom-7 -left-5" />
 
         <div className="grid grid-cols-1 z-20">
           <StatsInfoCard

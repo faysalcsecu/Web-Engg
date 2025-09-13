4fcc112eb48467844d87b6a71f4ef2ff071db86d
Faysal Bin Alauddin <122711816+faysalcsecu@users.noreply.github.com>
2025-09-14 00:25:14 +0600

    ..

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -3,7 +3,7 @@
 // export const BASE_URL = "https://web-engg.onrender.com";
 
 // export const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 // utils/apiPaths.js
 export const API_PATHS = {
   AUTH: {
